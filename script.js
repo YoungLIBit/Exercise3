77c51af4bea2ccc96183bf4713af3c00776d0e73
YoungLIBit <danghuutrong2016@gmail.com>
2024-03-24 01:43:54 +0700

    finish 2

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ $(document).ready(function (){
     step1.click(function (){
         step2.removeClass('active');
         step3.removeClass('active')
-        $('.content__process').removeClass('active2','active3');
+        $('.content__process').removeClass('active2 active3');
 
         $('.content__product').show();
         $('.content__txt').hide();
@@ -29,4 +29,65 @@ $(document).ready(function (){
         $('.content__product').hide();
         $('.content__txt').hide();
     })
+});
+
+$(document).ready(function(){
+    var preClickElement = null;
+    var sidebarList = [$('.sidebar__dashboard'),$('.sidebar__analyst'),$('.sidebar__ecommerce') ];
+    $.each(sidebarList,function (){
+        $(this).click(function (){
+            if(preClickElement !== null){
+                preClickElement.removeClass('sidebar-active');
+            }
+
+            $(this).addClass('sidebar-active');
+            preClickElement = $(this);
+        });
+    });
+});
+$(document).ready(function(){
+    var preClickElement = null;
+    $('.sidebar__ui-down').click(function(){
+        if(preClickElement !== null){
+            preClickElement.removeClass('sidebar-active');
+        }
+        $(this).addClass('sidebar-active');
+        preClickElement = $(this);
+    })
+});
+$(document).ready(function(){
+    $('label.sidebar__ui-grid').click(function(){
+        $(this).toggleClass('sidebar-active');
+        if($(this).hasClass('sidebar-active')){
+            $('.sidebar__ui-icon').hide();
+            $('.sidebar__ui-icon-down').show();
+            $('.sidebar_children').show();
+            var preElementActive = null;
+            $('.sidebar_children .sidebar__ui-grid').click(function (){
+                if(preElementActive !== null){
+                    preElementActive.removeClass('sidebar-active');
+                }
+                $(this).addClass('sidebar-active');
+                preElementActive = $(this);
+            });
+        }else{
+            $('.sidebar__ui-icon').show();
+            $('.sidebar__ui-icon-down').hide();
+            $('.sidebar_children').hide();
+        }
+    });
+});
+
+$(function (){
+    $('.content__sidebar-user-image').click(function(){
+        var userActive = 'content__sidebar-user-active';
+        $(this).toggleClass(userActive);
+        if($(this).hasClass(userActive)){
+            $('.user').show();
+        }else{
+            $('.user').hide();
+        }
+    })
+
+
 });
